69afcc0eec46c78b92b40ff3bac0387315c39c24
isaquealvesdeeaf <isaquealves5544@gmail.com>
2020-02-08 01:43:04 +0000

    add class cliente

--- exemplo/cliente.js
+++ exemplo/cliente.js
@@ -1,9 +1,42 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var empresa;
 (function (empresa) {
-    var cliente = /** @class */ (function () {
+    var cliente = /** @class */ (function (_super) {
+        __extends(cliente, _super);
         function cliente() {
+            return _super !== null && _super.apply(this, arguments) || this;
         }
+        cliente.prototype.getSaldo = function () {
+            return this.saldo;
+        };
+        cliente.prototype.setSaldo = function (saldo) {
+            this.saldo = saldo;
+        };
+        cliente.prototype.getLimite = function () {
+            return this.limite;
+        };
+        cliente.prototype.setLimite = function (saldo) {
+            this.limite = this.limite;
+        };
+        cliente.prototype.getCodCliente = function () {
+            return this.codCliente;
+        };
+        cliente.prototype.setCodCliente = function (CodCliente) {
+            this.codCliente = CodCliente;
+        };
         return cliente;
-    }());
+    }(empresa.pessoa));
 })(empresa || (empresa = {}));
--- exemplo/cliente.ts
+++ exemplo/cliente.ts
@@ -1,5 +1,32 @@
 namespace empresa{
-    class cliente{
-        
+    class cliente extends pessoa{
+        private saldo: number|undefined;
+        private limite: number|undefined;
+        private codCliente: number|undefined;
+
+        getSaldo():number|undefined{
+            return this.saldo;
+        }
+
+        setSaldo(saldo:number):void{
+            this.saldo = saldo;
+        }
+
+        getLimite():number|undefined{
+            return this.limite;
+        }
+
+        setLimite(saldo:number):void{
+            this.limite = this.limite;
+        }       
+         
+        getCodCliente():number|undefined{
+            return this.codCliente;
+        }
+
+        setCodCliente(CodCliente:number):void{
+            this.codCliente = CodCliente;
+        }
+               
     }
 }
